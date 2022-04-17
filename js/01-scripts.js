042a317c673f7eb70dc782d506295a37b2a5c697
cumbinar <cumbiweb@gmail.com>
2022-04-16 19:14:19 -0500

    acceder a html con document

---
+++ js/01-scripts.js
@@ -0,0 +1,48 @@
+//acceder a html con document //
+
+let elemento;
+
+elemento = document;
+console.log(elemento);
+
+elemento = document.all; //selecciona todo el dom//
+console.log(elemento);
+
+elemento = document.head;
+console.log(elemento);
+
+elemento = document.body;
+console.log(elemento);
+
+elemento = document.domain; //en qué dominio actual está cargando//
+
+
+elemento = document.forms; //formularios//
+console.log(elemento); 
+
+elemento = document.forms[0].id;
+console.log(elemento); 
+
+elemento = document.forms[0].classList;
+console.log(elemento);
+
+elemento = document.links;
+console.log(elemento);
+
+
+elemento = document.images;
+console.log(elemento);
+
+elemento = document.scripts;
+console.log(elemento);
+
+
+
+
+
+
+
+
+
+
+
