0a941ac0ec45f6da56254cda041e106cd31fc8d3
cumbinar <cumbiweb@gmail.com>
2022-04-17 12:57:50 -0500

    getElementById

---
+++ js/03-scripts.js
@@ -0,0 +1,7 @@
+//getElementById//
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+const noExiste = document.getElementById('no-existe'); //si no existe el id, se imprime null//
+console.log(noExiste);
