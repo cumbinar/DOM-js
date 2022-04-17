fac8931b226fd268d13116b1a503fa4759eea6f6
cumbinar <cumbiweb@gmail.com>
2022-04-17 12:47:21 -0500

    getElementsByClassName

---
+++ js/02-scripts.js
@@ -0,0 +1,12 @@
+//getElementsByClassName///
+
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+
+const hero = document.getElementsByClassName('hero');
+console.log(hero);
+
+const contenedores = document.getElementsByClassName('contenedor'); //selecciona todos los que tengan la clase contenedor//
+console.log(contenedores);
