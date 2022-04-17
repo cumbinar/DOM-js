36e0db2e3e8ee20a074d96cc3840194b206c73c1
cumbinar <cumbiweb@gmail.com>
2022-04-17 13:32:15 -0500

    querySelectorAll

---
+++ js/05-scripts.js
@@ -0,0 +1,9 @@
+//querySelectorAll selecciona todos los elementos que coincidan//
+
+const card = document.querySelectorAll('.card');
+console.log(card);
+
+
+const h2 = document.querySelectorAll('h2');
+console.log(h2.length);
+
