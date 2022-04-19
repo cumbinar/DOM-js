51e36f54dd5edf52ed6f284e4fb795c9969fdac1
cumbinar <cumbiweb@gmail.com>
2022-04-19 13:06:47 -0500

    finish modificar imágenes

--- js/06-scripts.js
+++ js/06-scripts.js
@@ -2,6 +2,13 @@
 
 const encabezado = document.querySelector('.contenido-hero h1');
 console.log(encabezado);
-console.log(encabezado.innerText);
+console.log(encabezado.innerText); //si en css visibility: hiden; no lo va encontrar//
 console.log(encabezado.textContent);
+console.log(encabezado.innerHTML); //trae todo el contenido html////
 
+
+document.querySelector('.contenido-hero h1').textContent = 'cambia el texto del h1';
+
+
+const imagen = document.querySelector('.card img');
+imagen.src = 'img/hacer2.jpg'; //cambia la imagen 1//
