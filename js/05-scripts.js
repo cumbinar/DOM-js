728b6a7dad701b422d9efaefe20a0674f359b981
cumbinar <cumbiweb@gmail.com>
2022-04-19 12:50:12 -0500

    Modificar Textos o Imagenes con JS

--- js/05-scripts.js
+++ js/05-scripts.js
@@ -7,3 +7,5 @@ console.log(card);
 const h2 = document.querySelectorAll('h2');
 console.log(h2.length);
 
+//Modificar Textos o Imagenes con JS//
+
---
+++ js/06-scripts.js
@@ -0,0 +1,7 @@
+//Modificar Textos o Imagenes con JS//
+
+const encabezado = document.querySelector('.contenido-hero h1');
+console.log(encabezado);
+console.log(encabezado.innerText);
+console.log(encabezado.textContent);
+
