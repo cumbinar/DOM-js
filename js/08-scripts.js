b14775a6890321dd7e228497f834c079192c254e
cumbinar <cumbiweb@gmail.com>
2022-04-20 16:47:53 -0500

    Traversing the DOM

---
+++ js/08-scripts.js
@@ -0,0 +1,6 @@
+//Traversing the DOM//
+
+const navegacion = document.querySelector('.navegacion');
+console.log(navegacion);
+console.log(navegacion.childNodes);//los espacios en blanco se consideran elementos//
+console.log(navegacion.children);
