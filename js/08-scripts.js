91793ca163dc151d8e5044faf13c0f1e955a232e
cumbinar <cumbiweb@gmail.com>
2022-04-20 17:11:38 -0500

    navegar y hacer cambios traversing the DOM

--- js/08-scripts.js
+++ js/08-scripts.js
@@ -1,6 +1,24 @@
 //Traversing the DOM//
+//para recorrer por el DOM//
 
 const navegacion = document.querySelector('.navegacion');
 console.log(navegacion);
 console.log(navegacion.childNodes);//los espacios en blanco se consideran elementos//
 console.log(navegacion.children);
+console.log(navegacion.children[1]);
+console.log(navegacion.children[1].nodeName);
+console.log(navegacion.children[1].nodeType);
+
+const card = document.querySelector('.card');
+console.log(card.children); //selecciona el card//
+console.log(card.children[1]); // selecciona el hijo//
+console.log(card.children[1].children); //selecciona uno  de los hijos//
+console.log(card.children[1].children[1]);//más específico//
+console.log(card.children[1].children[1].textContent);//aún más específico//
+
+card.children[1].children[1].textContent = 'cambio a texto Cumbi Tara';
+console.log(card.children[1].children[1].textContent); //cambia el texto seleccionado//
+
+card.children[0].src = 'img/hacer2.jpg' //cambia la imagen de la posición cero//
+console.log(card.children[0]);
+
