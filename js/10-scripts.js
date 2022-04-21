1cca2b94d566ab0b5e2e9a737c6c8fa46ac63604
cumbinar <cumbiweb@gmail.com>
2022-04-21 17:17:45 -0500

    insertar antes de un elemento de referencia

--- js/10-scripts.js
+++ js/10-scripts.js
@@ -1,7 +1,7 @@
 // Generar HTML con JavaScript //
 
 const enlace = document.createElement('a'); //creando enlace//
-enlace.textContent = 'nuevo-enlace'; //agregando texto//
+enlace.textContent = 'Nuevo-Enlace'; //agregando texto//
 enlace.href = '/nuevo-enlace'; //agregando href//
 enlace.target = '_blank'; 
 console.log(enlace);
@@ -9,4 +9,10 @@ console.log(enlace);
 //ahora se selecciona el sitio en donnde se va a posicionar//
 
 const navegacion = document.querySelector('.navegacion');
-navegacion.appendChild(enlace); //agrega el enlace//
+navegacion.appendChild(enlace); //agrega el enlace al final//
+
+const enlace2 = document.createElement('a'); //creando enlace//
+enlace2.textContent = 'Cumbi'; //agregando texto//
+enlace2.href = '/nuevo-enlace2'; //agregando href//
+enlace2.target = '_blank'; 
+navegacion.insertBefore(enlace2, navegacion.children[2]); //lo ubica antes del elemento de referencia//
