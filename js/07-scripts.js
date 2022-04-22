24e61e62b6c3a3e0d8dd90f2c0c40c7153e437b5
cumbinar <cumbiweb@gmail.com>
2022-04-22 11:31:05 -0500

    cambio backgroundColor

--- js/07-scripts.js
+++ js/07-scripts.js
@@ -2,3 +2,8 @@
 
 const encabezado = document.querySelector('h1');
 console.log(encabezado);
+
+encabezado.style.backgroundColor = 'red'; //cambia el background-color del h1//
+console.log(encabezado.style);
+
+
