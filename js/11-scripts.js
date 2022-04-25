05e46e3bfb60e7272117457f283054c2b939fc2d
cumbinar <cumbiweb@gmail.com>
2022-04-24 20:26:45 -0500

    addEventListener

--- js/11-scripts.js
+++ js/11-scripts.js
@@ -1,4 +1,4 @@
-//eventos en JavaScript//
+//eventos con JavaScript//
 
 
 const btnFlotante = document.querySelector('.btn-flotante');
@@ -6,4 +6,16 @@ const footer = document.querySelector('.footer');
 
 btnFlotante.addEventListener('click', () => {
     console.log('Diste click en el botón')
-})
+});
+
+btnFlotante.addEventListener('click', mostrarOcultarFooter);
+
+function mostrarOcultarFooter() {
+
+    if (footer.classList.contains('activo')) {
+        footer.classList.remove('activo');
+    } else {
+        footer.classList.add('activo');
+    }
+    
+}
