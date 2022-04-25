aeb6d7aec9d7c15187d825dc3be27afb71f69b79
cumbinar <cumbiweb@gmail.com>
2022-04-25 07:41:51 -0500

    DOMContentLoaded

---
+++ js/12-scripts.js
@@ -0,0 +1,9 @@
+//eventos Detectar cuando el HTML esta listo//
+
+console.log('1');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Documento HTML cargado 2'); 
+}) //espera a que todo el html está cargado//
+
+console.log('3');
