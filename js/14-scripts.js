85bff77dc5b56afe82c579888bdc229672260cb3
cumbinar <cumbiweb@gmail.com>
2022-04-27 17:42:33 -0500

    init Inputs

---
+++ js/14-scripts.js
@@ -0,0 +1,8 @@
+//Eventos sobre los Inputs: entradas con el teclado//
+
+const busqueda = document.querySelector('.busqueda'); //me ubico sobre la clase busqueda//
+
+busqueda.addEventListener('keydown', () => {
+    console.log('Escribiendo...')
+
+})
